850fbc137a8ed839d95b4362e06eab0c32aee304
Visualizeit <17688172112@163.com>
2024-07-07 13:03:36 +0800

    feat: update router configuration and add lazy loading for routes

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-const App = () => {
-	return <h1 className="text-5xl">Hello World</h1>
-}
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
+import { RouterProvider } from '@tanstack/react-router'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import router from './router'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 )
