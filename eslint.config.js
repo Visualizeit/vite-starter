20ec7e8cd76847636e35dd4a85f8897f0f549048
Visualizeit <17688172112@163.com>
2024-07-08 22:47:54 +0800

    chore: update eslint.config.js to include recommended jsxA11y rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import { config as defaultConfig } from '@epic-web/config/eslint'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 
 /** @type {import("eslint").Linter.FlatConfig} */
 export default [
 	...defaultConfig,
+	jsxA11y.flatConfigs.recommended,
 	{
 		ignores: ['src/routeTree.gen.ts'],
 	},
