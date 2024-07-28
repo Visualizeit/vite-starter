e41615a14eaafd2c1b22d93dbd79a808602995ad
Visualizeit <17688172112@163.com>
2024-07-28 23:23:33 +0800

    fix: update vite.config.ts to rearrange plugin order

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		tsconfigPaths(),
-		react(),
-		TanStackRouterVite({ experimental: { enableCodeSplitting: true } }),
-	],
+	plugins: [tsconfigPaths(), TanStackRouterVite(), react()],
 	server: {
 		open: true,
 	},
