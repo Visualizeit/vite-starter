97a4285b8896a5dbb67ae04c9f6c74c7a861d4b2
Visualizeit <17688172112@163.com>
2024-08-04 13:59:49 +0800

    chore: update eslint.config.js to use correct type import

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import { config as defaultConfig } from '@epic-web/config/eslint'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 
-/** @type {import("eslint").Linter.FlatConfig} */
+/** @type {import("eslint").Linter.Config} */
 export default [
 	...defaultConfig,
 	jsxA11y.flatConfigs.recommended,
