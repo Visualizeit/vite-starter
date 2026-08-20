ffa88d2c26a79654e316d8d07f806be17cb83ead
Visualizeit <vnyeit@gmail.com>
2026-08-20 19:17:44 +0800

    chore: use oxc react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact, { reactCompilerPreset } from '@vitejs/plugin-react'
+import viteReact from '@vitejs/plugin-react'
 import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite-plus'
 
@@ -11,8 +10,7 @@ export default defineConfig({
         tailwindcss(),
         tanstackStart(),
         nitro(),
-        viteReact(),
-        babel({ presets: [reactCompilerPreset()] }),
+        viteReact({ compiler: true }),
     ],
     resolve: {
         tsconfigPaths: true,
